b0d438f81a1f44fcbe2c97d87386690acbc64877
Mohamed Yassine <yassineaboub@live.fr>
2019-09-18 13:25:36 +0000

    second

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- * edit
+ * Print message
  */
 
 int main(void)
+
 {
   puts("\"Programming is like building a multilingual puzzle");
   return 0;
