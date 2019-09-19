7621066240480ed2f9bd639b1d4d4627f5f22766
Mohamed Yassine <yassineaboub@live.fr>
2019-09-19 22:14:03 +0000

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+if (c != 'q' && c != 'e')
+putchar(c);
+putchar('\n');
+
+return (0);
+}
