1a1935f4b790ba99da088d86f363e20c9a4c1e0e
Mohamed Yassine <yassineaboub@live.fr>
2019-09-19 22:29:32 +0000

    lol

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+
+for (c = 'a'; c <= 'z'; c++)
+if (c != 'q' && c != 'e'
+putchar(c);
+putchar('\n');
+
+return (0);
+}
+© 2019 GitHub, Inc.
