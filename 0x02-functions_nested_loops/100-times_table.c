1a3c92258ea92d070150331ed6573608e0654c6e
Mohamed Yassine <yassineaboub@live.fr>
2019-09-24 21:16:55 +0000

    ja

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+* print_times_table - prints the n times table, starting at 0.
+*
+* @n: input integer
+*
+* Returns - void
+*/
+void print_times_table(int n)
+{
+int a, b, ab, huns, tens, ones;
+
+if (!(n > 15 || n < 0))
+{
+for (a = 0; a <= n; a++)
+{
+if (n == 0)
+{
+_putchar('0');
+_putchar('\n');
+break;
+}
+for (b = 0; b <= n; b++)
+{
+if (b == 0)
+{
+_putchar(0 + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+ab = a * b;
+huns = (ab > 99) ? ab / 100 : -16;
+tens = (ab > 99) ? ab / 10  % 10 : (ab > 9) ? ab / 10 : -16;
+ones = ab % 10;
+
+_putchar(huns + '0'); /* -16 + '0' is a SPACE */
+_putchar(tens + '0'); /* -16 + '0' is a SPACE */
+_putchar(ones + '0');
+if (b == n)
+break;
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+
+/**
+* main - print the first 98 Fibonacci numbers, starting with 1 and 2. \n
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+float i, j, tmp;
+
+i = 1;
+j = 2;
+
+printf("%.0f", i);
+
+for (n = 0; n < 97; n++)
+{
+printf(", %.0f", j);
+tmp = i;
+i = j;
+j += tmp;
+}
+putchar('\n');
+
+return (0);
+}
+© 2019 GitHub, Inc.
