809fc9056e54f791b5600f680e46cd764d93b2c8
Mohamed Yassine <yassineaboub@live.fr>
2019-09-24 19:23:26 +0000

    heo

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_sign - prints the sign of a number
+*
+* @n: input integer tested
+*
+* Return: 1 or 0 or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+return (1);
+}
