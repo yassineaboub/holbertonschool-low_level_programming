aed01c96c0e69c74f2d04e9d346167397747593a
Mohamed Yassine <yassineaboub@live.fr>
2019-10-01 22:20:53 +0000

    flk

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,20 @@
 #include "holberton.h"
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
+* print_rev - function
+* @s: print a string
+* Always 0.
 */
 void print_rev(char *s)
 {
+int i, j;
+
+i = 0;
+while ((s[i]) != '\0')
+{
+i++;
+}
+for (j = i-1; j > 0; j--)
+_putchar(s[j]);
+_putchar('\n');
+}
