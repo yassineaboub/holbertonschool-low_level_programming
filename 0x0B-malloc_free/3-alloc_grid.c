ad625d43a52d4b4539d658641298750564473485
Mohamed Yassine <yassineaboub@live.fr>
2019-10-18 22:55:07 +0000

    sec

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+*alloc_grid - a pointer to 2D array
+*@width:int
+*@height:int
+*Return: (0 Success)
+*/
+
+int **alloc_grid(int width, int height)
+{
+int **ar;
+int i, j, x, y;
+
+
+if (width <= 0 || height <= 0)
+return (NULL);
+ar = malloc(height * sizeof(int *));
+if (ar == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+ar[i] = malloc(width * sizeof(int));
+if (ar[i] == NULL)
+{
+for (x = 0; x < i; x++)
+free(ar[x]);
+
+free(ar);
+
+return (NULL);
+}
+}
+
+for (y = 0; y < height; y++)
+{
+for (j = 0; j < width; j++)
+ar[y][j] = 0;
+}
+return (ar);
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* *argstostr - concats the arguments
+*@ac: int
+*@av: int
+*Return: 0 (success)
+*/
+char *argstostr(int ac, char **av)
+{
+int i, j, c;
+char *str, *beg;
+if (ac == 0 || av == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+c++;
+}
+c++;
+}
+c++;
+str = malloc(c *sizeof(char));
+
+beg = str;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+*str = av[i][j];
+str++;
+}
+*str = '\n';
+str++;
+}
+return (beg);
+}
