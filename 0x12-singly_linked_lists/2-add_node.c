f49d67daaf7871135543bdcf75c58cdf0879aab0
Mohamed Yassine <yassineaboub@live.fr>
2019-11-01 21:08:06 +0000

    ji

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,20 +4,20 @@
 * add_node - adds a new
 * @head: pointer to the start of the list
 * @str: a constant character pointer
-* Return: a pointer to the new structure
+* Return: a pointer to the new struct
 */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-int len = 0;
-new = malloc(sizeof(list_t));
+size_t i = 0;
+list_t *new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
-for (; str[len]; len++)
-;
-new->str = strdup(str);
-new->len = len;
 new->next = *head;
+new->str = strdup(str);
+if (new->str)
+while (new->str[i])
+++i;
+new->len = i;
 *head = new;
 return (new);
 }
